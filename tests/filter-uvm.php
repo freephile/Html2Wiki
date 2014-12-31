1cb32e2901cbac8c59cb0f9aebcd5a480a89c570
Greg Rundlett <greg@freephile.com>
2014-12-31 00:22:20 -0500

    adds regex test adds PHP DOM test neither are successful

--- tests/filter-uvm.php
+++ tests/filter-uvm.php
@@ -17,13 +17,33 @@
  * @file
  */
 
-$filename = "uvm-1.1d-docs-html-files-overviews-intro-txt";
-$raw = file_get_contents($filename);
 
-// define a pattern to remove <script> tags
-$reScript = "#<script\b[^>]*>(.*?)</script>#is"; // lazy dot-all
+$filename = "uvm-1.1d-docs-html-files-overviews-intro-txt.html";
+$raw = file_get_contents(__DIR__ . "/$filename");
 
-$html = preg_replace($reScript, '', $raw);
 
+// The regex approach fails on a simple test.  
+// It strips the closing </body></html> tags when it shouldn't
+// a pattern to remove <script> tags
+$reScript = "#<script\b[^>]*>(.*?)</script>#ius"; // lazy dot-all
+// echo $html = preg_replace($reScript, '', $raw);
+
+// @link http://docs.php.net/manual/en/domdocument.loadhtml.php PHP DOMDocument 
+// which is based on libxml
+// As the documentation points out, you should handle the many errors and warnings
+// this tool will generate.
+$doc = new DOMDocument();
+// suppress the frailty of libxml
+libxml_use_internal_errors(true);
+$doc->loadHTML($raw);
+$scriptTags = $doc->getElementsByTagName('script');
+$length = $scriptTags->length;
+// for each tag, remove it from the DOM
+for ($i = 0; $i < $length; $i++) {
+  $scriptTags->item($i)->parentNode->removeChild($scriptTags->item($i));
+}
+// get the HTML string back
+$html = $doc->saveHTML();
 echo $html;
+
 
