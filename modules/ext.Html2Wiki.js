71aff56ddc3c6d8fe481bd193e49c88325f8e705
Greg Rundlett <greg@freephile.com>
2015-01-14 10:36:00 -0500

    work on AJAX

--- modules/ext.Html2Wiki.js
+++ modules/ext.Html2Wiki.js
@@ -1,7 +1,7 @@
 /**
- * JavaScript for Foo in Html2Wiki.
+ * JavaScript for Html2Wiki.
  */
-
+/** More modern example, but without any defintion
 ( function ( mw, $ ) {
 	var foo, x, y;
 
@@ -21,3 +21,36 @@
 	mw.libs.foo = foo;
 
 }( mediaWiki, jQuery ) );
+*/
+/** 
+ * 
+ * practical example to get working
+ */
+
+// Execute this after the site is loaded.
+jQuery(document).ready(function ($) {
+    // Find list items representing folders and
+    // style them accordingly.  Also, turn them
+    // into links that can expand/collapse the
+    // tree leaf.
+    $('li > ul').each(function (i) {
+        // Find this list's parent list item.
+        var parent_li = $(this).parent('li');
+ 
+        // Style the list item as folder.
+        parent_li.addClass('folder');
+ 
+        // Temporarily remove the list from the
+        // parent list item, wrap the remaining
+        // text in an anchor, then reattach it.
+        var sub_ul = $(this).remove();
+        parent_li.wrapInner('<a>').find('a').click(function () {
+            // Make the anchor toggle the leaf display.
+            sub_ul.toggle();
+        });
+        parent_li.append(sub_ul);
+    });
+ 
+    // Hide all lists except the outermost.
+    $('ul ul').hide();
+});
