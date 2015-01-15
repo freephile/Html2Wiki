de65c19ff4600ea0faaafe77187eeaede8a936b7
Greg Rundlett <greg@freephile.com>
2015-01-15 09:12:09 -0500

    introduces mw.config.get and how to get an edit token

--- modules/ext.Html2Wiki.js
+++ modules/ext.Html2Wiki.js
@@ -1,6 +1,101 @@
 /**
  * JavaScript for Html2Wiki.
  */
+// Apparently these are not necessary in the newest mw, or at least not in my module
+// var $ = jQuery;
+// var mw = mediaWiki;
+
+// Access multiple config values for use throughout our code base
+// https://www.mediawiki.org/wiki/Manual:Interface/JavaScript#mw.config
+var conf = mw.config.get([
+	'wgServer',
+	'wgPageName',
+	'wgCanonicalSpecialPageName',
+	'wgUserLanguage',
+	'wgVersion' // The only one we're using at the moment
+]);
+
+var edittoken = mw.user.tokens.get( 'editToken' );
+// alert( 'According to the api, you have an edit token of ' + edittoken );
+
+
+/**
+ * getToken();
+ * A lot harder way of getting an edit token
+ * @returns {undefined}
+ */
+function getToken () {
+	var mwVersion = parseInt( conf.wgVersion.substr( 2, 2 ) );
+	// if we're using mw 1.24 or greater, use action=query&meta=tokens to get a token
+	if ( mwVersion > 24 ) {
+		$.ajax({ url: mw.util.wikiScript( 'api' ), dataType: 'json', type: 'POST',
+		data: {
+			format: 'json',
+			action: 'query',
+			meta: 'tokens'
+		}, success: function ( data ) {
+			if ( data && data.query && data.query.tokens ) {
+				var tokens = data.query.tokens;
+				var token = tokens.csrftoken;
+				alert (token);
+			}
+		}, error: function () {
+			alert('no token');
+		}});
+	} else {
+		alert('old version of MediaWiki ' + mwVersion);
+	}
+}
+
+
+var api = new mw.Api();
+/*
+var summary = "This is a summary";
+var content = "Hello World";
+addNewArticle(summary, content);
+
+function addNewArticle( summary, content ) {
+	api.postWithToken( "edit", {
+		action: "edit",
+		title: "NewPage",
+		summary: summary,
+		text: content,
+		token: edittoken
+	} ).done( function( result, jqXHR ) {
+		mw.log("Saved new Article at NewPage");
+		location.reload();
+	} ).fail( function( code, result ) {
+		if ( code === "http" ) {
+			mw.log( "HTTP error: " + result.textStatus ); // result.xhr contains the jqXHR object
+		} else if ( code === "ok-but-empty" ) {
+			mw.log( "Got an empty response from the server" );
+		} else {
+			mw.log( "API error: " + code );
+		}		
+	} );
+}
+*/
+
+function addNewSection( summary, content ) {
+	api.postWithToken( "edit", {
+		action: "edit",
+		title: mw.config.get( "wgPageName" ),
+		section: "new",
+		summary: summary,
+		text: content
+	} ).done( function( result, jqXHR ) {
+		mw.log( "Saved successfully" );
+		location.reload();
+	} ).fail( function( code, result ) {
+		if ( code === "http" ) {
+			mw.log( "HTTP error: " + result.textStatus ); // result.xhr contains the jqXHR object
+		} else if ( code === "ok-but-empty" ) {
+			mw.log( "Got an empty response from the server" );
+		} else {
+			mw.log( "API error: " + code );
+		}
+	} );
+}
 /** More modern example, but without any defintion
 ( function ( mw, $ ) {
 	var foo, x, y;
@@ -22,6 +117,7 @@
 
 }( mediaWiki, jQuery ) );
 */
+
 /** 
  * 
  * practical example to get working
