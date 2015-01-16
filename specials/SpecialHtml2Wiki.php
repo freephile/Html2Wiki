42cbec6de7fe59d4482a79c2b6fec48207ded138
Greg Rundlett <greg@freephile.com>
2015-01-16 15:48:17 -0500

    adds Tidy processing tidyUp(); cleanUVMFile(); adds comments; showRaw(), listFile(), makeTitle(); change addModules() to reflect resourceloader; standardize a bit on element naming v. message naming v. function naming in js and php

--- specials/SpecialHtml2Wiki.php
+++ specials/SpecialHtml2Wiki.php
@@ -6,16 +6,27 @@
  * @file
  * @ingroup Extensions
  * @package Html2Wiki
+ * 
+ * @todo implement style guide https://www.mediawiki.org/wiki/Design/Living_style_guide
  */
 
 class SpecialHtml2Wiki extends SpecialPage {
     
 	/** @var string The HTML we want to turn into wiki text */
     private $mContent;
+    private $mContentRaw;  // the exact contents which were uploaded
+    private $mContentTidy; // Raw afer it's passed through tidy
+    private $mFile;        // The (input) HTML file
     /** @var string The (original) name of the uploaded file */
     private $mFilename;
     /** @var int The size, in bytes, of the uploaded file. */
     private $mFilesize;
+    
+    private $mIsTidy;      // @var bool true once passed through tidy
+    private $mTidyErrors;  // the error output of tidy
+    private $mTidyConfig;  // the configuration we want to use for tidy.
+            
+
 
     /** @todo review and cull the properties that we use here
      * These properties were copied from Special:Upload assuming they'd be 
@@ -120,6 +131,15 @@ class SpecialHtml2Wiki extends SpecialPage {
 	 * Get data POSTed through the form and assign them to the object
 	 * @param WebRequest $request Data posted.
 	 * We'll use the parent's constructor to instantiate the name but not perms
+     * 
+     * @todo We need to check for $wgEnableWriteAPI=true (default) because if it's not
+     * then our extension will not be able to do it's work.  There are two possible
+     * errors:
+     * noapiwrite: Editing of this wiki through the API is disabled. Make sure the $wgEnableWriteAPI=true; statement is included in the wiki's LocalSettings.php file
+     * writeapidenied: You're not allowed to edit this wiki through the API
+     * 
+     * The first is clear enough to the user what they have to do to fix it.
+     * The second is that they are not configured properly in the correct group
      */
     public function __construct($request = null) {
 		$name='Html2Wiki';
@@ -286,8 +306,10 @@ class SpecialHtml2Wiki extends SpecialPage {
             );
             return false;
         }
+        // Don't do this because the file goes away
+        //$this->mFile = $_FILES['userfile']['tmp_name'];
         $this->mFilename = $_FILES['userfile']['name'];
-        $this->mContent = file_get_contents($_FILES['userfile']['tmp_name']);
+        $this->mContentRaw = $this->mContent = file_get_contents($_FILES['userfile']['tmp_name']);
         $this->mFilesize = $_FILES['userfile']['size'];
         return true;
     }
@@ -301,38 +323,66 @@ class SpecialHtml2Wiki extends SpecialPage {
             );
             return false;
         }
+        $this->mFile = $file;
         $this->mFilename = basename($file);
-        $this->mContent = file_get_contents($file);
+        $this->mContentRaw = file_get_contents($file);
         $this->mFilesize = filesize($file);
         return true;
     }
+    
+    /**
+     * Not sure when we'll use this, but the intent was to create
+     * an ajax interface to manipulate the file like wikEd
+     */
+    private function showRaw() {
+        $out = $this->getOutput();
+        $out->addModules( array('ext.Html2Wiki') ); // add our javascript and css
+        //$this->mContent = $this->findBody();
+        $out->addHTML('<div class="mw-ui-button-group">'
+                . '<button class="mw-ui-button mw-ui-progressive" '
+                . 'form="html2wiki-form" formmethod="post" id="h2wWand" name="h2wWand">'
+                . '<img src="/w/extensions/Html2Wiki/modules/images/icons/wand.png" '
+                . 'alt="wand"/></button>'
+                . '</div><div style="clear:both"></div>');
+        $out->addHTML('<div id="h2wContent">' . $this->mContentRaw . '</div>');
+    }
+    
     /**
      * displays $this->mContent (in a <source> block)
      * optionally passed through htmlentities() and nl2br()
      */
     private function showContent($showEscaped=false) {
         $out = $this->getOutput();
-        $out->addModules( array('ext.Html2Wiki', 'ext.Html2Wiki.clickable') ); // add our javascript and css
-        // $out->addHTML('<pre>' . print_r($_FILES) . '</pre>');
-        //$out->addHTML('<ul class="mw-ext-Html2Wiki"><li>' . $this->mFilename . '</li><ul class="mw-ext-Html2Wiki"><li>2.one</li><li>2.two</li></ul></ul>');
-        $out->addHTML('<ul class="mw-ext-Html2Wiki"><li>' . $this->mFilename . '</li></ul>');
-        //$this->mContent = $this->findBody();
+        $out->addModules( array('ext.Html2Wiki') ); // add our javascript and css
+        
+        // @todo Error or warn here if not $mIsTidy or $mIsPure
+        
         if($showEscaped) {
             $escapedContent = $this->escapeContent();
-            $out->addHTML('<div id="h2w-label">Escaped File Contents:</div>');
-            $out->addHTML('<div id="h2w-content">' . $escapedContent . '</div>');
+            $out->addHTML('<div id="h2wLabel">Escaped File Contents:</div>');
+            $out->addHTML('<div id="h2wContent">' . $escapedContent . '</div>');
         } else {
-            $out->addHTML('<div id="h2w-label">Original File Contents:</div>');
+            $out->addHTML('<div id="h2wLabel">Original File Contents:</div>');
             // putting the original source into GeSHi makes it "safe" from the parser
-            // $out->addHTML(<div>$this->mContent</div>);
-            $out->addWikiText('<source id="h2w-content" lang="html4strict">' . $this->mContent . '</source>');
+            // but destroys any ability to act on the 'source' by manipulating this 
+            // elements innerHTML
+            $out->addWikiText('<source id="h2wContent" lang="html4strict">' . $this->mContent . '</source>');
         }
     }
     
-    private function findBody() {
-        
+    private function listFile() {
         $out = $this->getOutput();
-        $content = $this->mContent;
+        $out->addModules( array('ext.Html2Wiki') ); // add our javascript and css
+        $out->addHTML('<ul class="mw-ext-Html2Wiki"><li>' . $this->mFilename . '</li></ul>');
+    }
+    
+    /** Don't really need this because we're doing it with Tidy
+     * Tidy will correct errors AND give us the body
+     * @return type
+     */
+    private function findBody() {
+        $out = $this->getOutput();
+        $content = $this->mContentRaw;
         $pattern = '#<body[^>]*>(.*)</body>#';
         $foundBody = preg_match_all($pattern, $content, $matches);
         if ($foundBody && count($matches) === 2) {
@@ -348,10 +398,7 @@ class SpecialHtml2Wiki extends SpecialPage {
     private function escapeContent() {
         return nl2br(htmlentities($this->mContent, ENT_QUOTES | ENT_IGNORE, "UTF-8"));
     }
-    private function doTidy() {
-
-        
-    }
+    
 	/**
 	 * Do the import which consists of three phases:
      * 1) Select and/or Upload user nominated files to a temporary area so that
@@ -369,33 +416,65 @@ class SpecialHtml2Wiki extends SpecialPage {
         // We'll either be able to insert that programmatically
         // or use OutputPage->addWikiText() to make it appear in the page output for initial testing
         
+        $tidyOpts = array(
+            "drop-empty-paras" => 1,	
+            "drop-font-tags" => 1, 
+            "enclose-block-text" => 1, 
+            "enclose-text" => 1, 
+            "fix-backslash" => 1, 
+            "fix-bad-comments" => 1, 
+            "fix-uri" => 1, 
+            "hide-comments" => 1, 
+            "merge-divs" => 1, 
+            "merge-spans" => 1, 
+            "repeated-attributes" => "keep-first", 
+            "show-body-only" => 1, 
+            "show-errors" => 0, 
+            "show-warnings" => 0, 
+
+            "indent" => 0, 
+            "wrap" => 120, 
+            "tidy-mark" => 0, 
+            "write-back" => 0
+        );
+
         
-        // if($this->doUpload()) {
+        if($this->doUpload()) {
         // if($this->doLocalFile("/vagrant/mediawiki/extensions/Html2Wiki/data/uvm-1.1d/docs/html/files/base/uvm_printer-svh.html")) {
-        if($this->doLocalFile("/vagrant/mediawiki/extensions/Html2Wiki/data/docs/htmldocs/mgc_html_help/overview04.html")) {
+        // if($this->doLocalFile("/vagrant/mediawiki/extensions/Html2Wiki/data/docs/htmldocs/mgc_html_help/overview04.html")) {
+            $this->listFile();
+            
+            $this->tidyup($tidyOpts);
+            $this->cleanUVMFile();
+            // $this->showRaw();
             $this->showContent();
-            $this->saveArticle();
+            // $this->saveArticle();
         } else {
             $this->showForm();
         }
-        
-        
-        // $this->tidyup();
-        
-
         return true;
+    }
+    
+    private function makeTitle ( $namespace = NS_MAIN ) {
+        $desiredTitleObj = Title::makeTitleSafe( $namespace, $this->mFilename );
+        if (!is_null($desiredTitleObj)) {
+            return $desiredTitleObj;
+        } else {
+            die($this->mFilename . " is an invalid filename");
+        }
     }
     
     private function saveArticle () {
         $user = $this->getUser();
         $token = $user->editToken();
+        $title = $this->makeTitle( NS_MAIN );
         $api = new ApiMain(
             new DerivativeRequest(
                 $this->getRequest(), // Fallback upon $wgRequest if you can't access context
                 array(
                     'action'     => 'edit',
-                    'title'      => 'NewPage',
-                    'text'       => 'Hello World',
+                    'title'      => $title,
+                    'text'       => $this->mContent,
                     // 'appendtext' => '[[Category:UVM-1.1]]',
                     'summary'    => 'This is a summary',
                     'notminor'   => true,
@@ -420,10 +499,8 @@ class SpecialHtml2Wiki extends SpecialPage {
 		$action = $this->getPageTitle()->getLocalURL( array( 'action' => 'submit' ) );
 		$user = $this->getUser();
 		$out = $this->getOutput();
-        $out->addModules( array('ext.Html2Wiki', 'ext.Html2Wiki.clickable') ); // add our javascript and css
-        $out->addWikiMsg('html2wiki-intro');
-		$out->addHTML( '<div id="hello">Hello World</div>' );
-        
+        $out->addModules( array('ext.Html2Wiki') ); // add our javascript and css
+        $out->addWikiMsg('html2wiki-intro');       
         // display an error message if any
         if ($message) {
             $out->addHTML('<div class="error">' . $message . "</div>\n");
@@ -479,31 +556,106 @@ class SpecialHtml2Wiki extends SpecialPage {
 		}
 		
 	}
-    
-    public function tidyup () {
-        if (class_exists('Tidy')) {
-            // cleanup output
-            $config = array(
+
+    /**
+     * The Tidy class can easily use an array.
+     * To REUSE that same array in a command line environment, we reformat it
+     * @param array $tidyOpts
+     * @return string
+     */
+    private function makeConfigStringForTidy($tidyOpts) {
+        $tidyOptsString = '';
+        foreach ( $tidyOpts as $k => $v ) {
+            $tidyOptsString .= " --$k $v";
+        }
+        return $tidyOptsString;
+    }
+
+    /**
+     * Tidyup will populate both mContent and mContentTidy 
+     * and store errors in mTidyErrors
+     * 
+     * @param string | array $tidyConfig
+     * if passed as an array like so
+            $tidyConfig = array(
                 'indent'        => false,
                 'output-xhtml'  => true,
                 'wrap'          => 80
             );
+     * then tidy will use the supplied configuration
+     * Or, if passed as a string, tidy will use the configuration file
+     * Left alone, it will load the config file supplied with Html2Wiki
+     * 
+     * On success, populate $this->mIsTidy = true
+     * @return boolean
+     * 
+     * In testing tidy from the command line, it would NOT use a configuration file
+     * If you have an existing tidy.conf you want to use, you can convert a config
+     * awk '{ printf " --"$0 }' /vagrant/mediawiki/extensions/Html2Wiki/tidy.conf
+     */
+    public function tidyup ($tidyConfig = NULL) {
+        $this->mTidyConfig = realpath( __DIR__  . "/../tidy.conf");
+        if (is_null($tidyConfig)) {
+            $tidyConfig = $this->mTidyConfig;
+            $shellConfig = " -config $tidyConfig";
+        } elseif (is_array($tidyConfig)) {
+            $shellConfig = $this->makeConfigStringForTidy($tidyConfig);
+        } elseif (is_string($tidyConfig)) {
+            $shellConfig = " -config $tidyConfig";
+            if ( !is_readable($tidyConfig) ) {
+                // echo "Tidy's config not found, or not readable\n";
+            }
+        }
+        if (class_exists('Tidy')) {
             $encoding = 'utf8';
             $tidy = new Tidy;
-            $tidy->parseString($this->mContent, $config, $encoding);
+            $tidy->parseString($this->mContentRaw, $tidyConfig, $encoding);
             $tidy->cleanRepair();
+            // @todo need to do something else with this error list?
             if(!empty($tidy->errorBuffer)) {
-              echo "The following errors or warnings occurred:\n";
-              echo $tidy->errorBuffer;
+                $this->mTidyErrors = $tidy->errorBuffer;
             }
             // just focus on the body of the document
-            $this->mContent = (string) $tidy->body();
-            // convert the object to string
-            // $tidy = (string) $tidy;
-            return true;
+            $this->mContentTidy = $this->mContent = (string) $tidy->body();
         } else {
-            die('unable to load Tidy');
+            $tidy = "/usr/bin/tidy";
+            // only by passing the options as a long string worked.
+            $cmd= "$tidy -quiet -indent -ashtml $shellConfig {$_FILES['userfile']['tmp_name']}";
+            $escaped_command = escapeshellcmd($cmd);
+            if ( !is_readable($_FILES['userfile']['tmp_name']) ) {
+                echo "Tidy's target not found, or not readable\n";
+            }
+            echo "executing $escaped_command";
+            $this->mContentTidy = $this->mContent = shell_exec($escaped_command);
         }
+        $this->isTidy = true;
+        return true;
     }
+    
+    public function cleanUVMFile () {
+       // delete content tags
+        $reHead = "#<head>.*?</head>#is";
+        $reBody = "#<body\b[^>]*>(.*?)</body>#is";
+        $reScript = "#<script\b[^>]*>(.*?)</script>#is"; // caseless dot-all
+        $reComment = "#<!--(.*?)-->#s"; 
+        $reEmptyAnchor = "#<a\b[^>]*></a>#is"; // empty anchor tags
+        $reCollapsePre = '#</pre>.*?<pre class="pCode">#s'; // sibling pre tags
+        $reBlankLine = "#^\s?$\n#m";
+        // simulate Tidy by ditching the <head> and getting only the <body>
+        // but the source HTML could still be in a hurtful mess
+        if (!$this->isTidy) {
+            $this->mContent = preg_replace($reHead, '', $this->mContent);
+            $this->mContent = preg_filter($reBody, "$1", $this->mContent);
+        }
+        $this->mContent = preg_replace($reScript, '', $this->mContent);
+        $this->mContent = preg_replace($reEmptyAnchor, '', $this->mContent);
+        $this->mContent = preg_replace($reCollapsePre, '', $this->mContent);
+        
+        $this->mContent = preg_replace($reComment, '', $this->mContent);
+        // keep this last, because there will be a lot of blank lines generated
+        $this->mContent = preg_replace($reBlankLine, '', $this->mContent);
+
+    }
+    
 }
 
