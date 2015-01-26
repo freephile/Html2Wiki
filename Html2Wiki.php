70cb98b3364b070e699c0eac9c9cb550e6bf3578
Greg Rundlett <greg@freephile.com>
2015-01-25 21:31:37 -0500

    adds global options of $wgH2WEliminateDuplicateImages and $wgH2WProcessImages
    $wgH2WEliminateDuplicateImages boolean true
           - when set to true, imported image (and their references)
             will be flattened to
             [CollectionName/]image.jpg
             instead of
             [CollectionName/]nested/folder/image.jpg
             thus eliminating duplicates based on repeated nested folders
      $wgH2WProcessImages boolean true
           - when set to false, image processing will be skipped, saving time in the
             re-upload of content when no images have changed.
             With a simple preference option, we avoid doing a binary diff on every image.
             @todo create preference element in form

--- Html2Wiki.php
+++ Html2Wiki.php
@@ -81,6 +81,25 @@ $wgExtensionCredits['other'][] = array(
 
 /* Setup */
 define( 'HTML2WIKI_VERSION', '0.1' );
+
+/* Options:
+ * $wgH2WEliminateDuplicateImages boolean true
+ *      - when set to true, imported image (and their references)
+ *        will be flattened to 
+ *        [CollectionName/]image.jpg 
+ *        instead of 
+ *        [CollectionName/]nested/folder/image.jpg 
+ *        thus eliminating duplicates based on repeated nested folders
+ * $wgH2WProcessImages boolean true
+ *      - when set to false, image processing will be skipped, saving time in the 
+ *        re-upload of content when no images have changed.
+ *        With a simple preference option, we avoid doing a binary diff on every image.
+ *        @todo create preference element in form
+ */
+global $wgH2WEliminateDuplicateImages, $wgH2WProcessImages;
+$wgH2WEliminateDuplicateImages = true;
+$wgH2WProcessImages = true; 
+
 // Register files
 $dir = __DIR__;
 $wgAutoloadClasses['Html2WikiHooks'] = "$dir/Html2Wiki.hooks.php";
